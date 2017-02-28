041308fc29462e8edb166ca979bc05f9d5d9f6a7
Ivan <wcy5211678@163.com>
2017-02-28 16:35:52 +0800

    daojia

--- src/router/index.js
+++ src/router/index.js
@@ -12,9 +12,9 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
-    },
-    {
-      path: '/me'
     }
+    // {
+    //   path: '/me'
+    // }
   ]
 })
